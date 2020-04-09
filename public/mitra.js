047b98d62db8fcb3aa2c8dacf897beff383154ae
arfan <48755707+arfan21@users.noreply.github.com>
2020-04-09 22:56:48 +0700

    add sort by date

--- public/mitra.js
+++ public/mitra.js
@@ -12,7 +12,7 @@ appang.controller("menjadimitraCtrl",['$scope','$http','$window', function($scop
             data : $scope.data,
         }).then(function successCallback(response){
             console.log(response);
-            window.location.href = "https://api.whatsapp.com/send?phone=6289635639022&text=%2aGetPrint%2a%20%0ANama%20Toko%20%3A%20"+$scope.data.namatoko+"%2C%0ANama%20Pemilik%20%3A%20"+$scope.data.namapemilik+"%2C%0AAlamat%20Toko%20%3A%20"+$scope.data.alamattoko+"%2C%0ASaya%20tertarik%20untuk%20bergabung%20dengan%20mitra%20anda.";
+            window.location.href = "/";
         })
     }
 }]);
