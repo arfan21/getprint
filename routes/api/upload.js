74d291d541dc60360025cd9c27025ebe6667b1fc
arfan <48755707+arfan21@users.noreply.github.com>
2020-06-13 06:18:05 +0700

    auto send data pesanan ke line pemesan

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -5,6 +5,7 @@ const path   = require('path');
 const Upload = require("../../models/Upload");
 const mongoose = require('mongoose');
 const request = require("request");
+const axios = require("axios")
 const fs = require("fs")
 
 const storage = multer.diskStorage({
@@ -19,32 +20,45 @@ const upload = multer({storage : storage}).array('myfile',5);
 router.post("/uploadfile",upload, (req,res) => {
     var newfile = [];
 
-    for(i = 0; i < req.files.length;i++){
+    //looping untuk mengupload satu persatu file ke dropbox
 
+    for(i = 0; i < req.files.length;i++){
         let filename = req.files[i].filename;
         request({
             method: "POST",
             url : "https://content.dropboxapi.com/2/files/upload",
             headers: {
                 'Content-Type': 'application/octet-stream',
-                'Authorization' : 'Bearer <token>',
+                'Authorization' : 'Bearer ROeWeTjqfBAAAAAAAAAAUkKjzCS2pmeDufip5j0cRareedsNLp_H1KLHLQjj71AU',
                 'Dropbox-API-Arg' : "{\"path\": \"/getprint/"+filename+"\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}",
             },
             body : fs.readFileSync(path.join(__dirname + './../../public/file4print/' + filename))
         }, (err, response, body) => {
             let resJson = JSON.parse(body)
+
+            //melakukan request ke dropbox untuk membuat shared link dari file yang sudah di upload
+
             request({
                 method: "POST",
                 url : "https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings",
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization' : 'Bearer <token>',
+                    'Authorization' : 'Bearer ROeWeTjqfBAAAAAAAAAAUkKjzCS2pmeDufip5j0cRareedsNLp_H1KLHLQjj71AU',
                 },
                 body : "{\"path\": \""+resJson.path_display+"\"}",
             }, (err, response, body) => {
                 resJson = JSON.parse(body)
+
+                //shared link di simpan di array newfile
+
                 newfile.push(resJson.url)
+
+                //jika panjang array new file == panjang dari request file (atau sudah di akhir looping)
+
                 if(newfile.length == req.files.length){
+
+                    //shared link disimpan ke mongodb
+                    
                     const newUpload = new Upload({
                         id_pesanan : '5edcde17fee7e00004677cf9',
                         link_file : newfile,
@@ -67,37 +81,8 @@ router.post("/uploadfile",upload, (req,res) => {
                     })
                 }
             })
-
         })
-  
     }
-    
-    // const newUpload = new Upload({
-    //     id_pesanan : '5edcde17fee7e00004677cf9',
-    //     file : newfile,
-    // })
-
-    // newUpload.save()
-    //     .then(upload => console.log(upload))
-    //     .catch(err => console.log(err));
-
-    // request({
-    //     url : "https://getprint.herokuapp.com/api/pesanan/byid/" + req.body.pesananid,
-    //     json : true,
-    // }, (err, response, body) => {
-    //     var data = body.pesanan
-    //     var link
-    //     var linkconv = []
-    //     for(i = 0; i < newUpload.file.length; i++){
-    //         link = "https://getprint.herokuapp.com" + newUpload.file[i].link
-    //         linkconv.push(encodeURI(link)) 
-    //     }
-    //     return res.status(200).json({status : true,
-    //             message : 'Success upload file',
-    //             data : data,
-    //             link : linkconv});
-    // })
-     
 })  
 
 module.exports = router;
