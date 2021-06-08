d8e7a60e4b45473658855282e59ea81c101aacc5
arfan21 <arfan.jadul10@gmail.com>
2021-06-08 18:33:27 +0700

    fix mongoUri

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
-const bodyparser = require("body-parser");
-const mongoose = require("mongoose");
-const db = require("./keys").mongoURI;
-const admin = require("./routes/api/admin");
-const router = require("./routes/api/routes");
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const admin = require('./routes/api/admin');
+const router = require('./routes/api/routes');
 
 mongoose
-    .connect(db, {
+    .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
     })
-    .then(() => console.log("mongoDB Connected"))
+    .then(() => console.log('mongoDB Connected'))
     .catch((err) => console.log(err));
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 //route api
-app.use(express.static("public"));
-app.use("/api/", admin);
-app.use("/api/", router);
+app.use(express.static('public'));
+app.use('/api/', admin);
+app.use('/api/', router);
 
-app.get("/send-id", function (req, res) {
+app.get('/send-id', function (req, res) {
     res.json({ id: myLiffId });
 });
 
-app.get("*", function (req, res) {
-    if (req.accepts("html")) {
+app.get('*', function (req, res) {
+    if (req.accepts('html')) {
         res.send(
-            "404",
-            '<script>location.href = "/pagenotfound.html";</script>'
+            '404',
+            '<script>location.href = "/pagenotfound.html";</script>',
         );
         return;
     }
